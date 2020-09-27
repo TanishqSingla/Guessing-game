2f7d6958c063f98a95a3e553a8b29235f26021ab
TanishqSingla <tanishqsingla00@gmail.com>
2020-09-28 00:50:08 +0530

    Generating a random number and added game logic

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 let input = document.querySelector("input");
 let button = document.querySelector("button");
 
+let rand = Math.floor(Math.random() * 101); //generating number between 0 - 101
+
+console.log(rand);
+
 function check() {
-  console.log(input.value);
+  let inputVal = +input.value; //getting value as a number
+  if (inputVal < rand) {
+    console.log("Your guess is lower than the number");
+  } else if (inputVal > rand) {
+    console.log("Your value is higher than the number");
+  } else if (inputVal === rand) {
+    console.log("Hurray! You guessed the correct number");
+  }
 }
 
 button.addEventListener("click", check);
