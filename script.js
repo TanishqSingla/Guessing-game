14eb35024e985fcb4b56bb0259965caaf346f8e9
TanishqSingla <tanishqsingla00@gmail.com>
2020-09-28 01:06:16 +0530

    Showing output in a paragraph instead of console also resetting its value on reset

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let input = document.querySelector("input");
 let submit = document.getElementById("submit");
 let reset = document.getElementById("reset");
+let p = document.querySelector("p");
 
 let rand = Math.floor(Math.random() * 101); //generating number between 0 - 101
 
@@ -9,18 +10,18 @@ console.log(rand);
 function check() {
   let inputVal = +input.value; //getting value as a number
   if (inputVal < rand) {
-    console.log("Your guess is lower than the number");
+    p.innerHTML = "Your guess is lower than the number";
   } else if (inputVal > rand) {
-    console.log("Your value is higher than the number");
+    p.innerHTML = "Your value is higher than the number";
   } else if (inputVal === rand) {
-    console.log("Hurray! You guessed the correct number");
+    p.innerHTML = "Hurray! You guessed the correct number";
   }
 }
 
 function Reset() {
   input.value = "";
   rand = Math.floor(Math.random() * 101); //generating a new number;
-  console.log(rand);
+  p.innerHTML = "";
 }
 
 submit.addEventListener("click", check);
