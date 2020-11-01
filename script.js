ec63f1887943e61e073e47e037c89af2a50e94b9
TanishqSingla <tanishqsingla00@gmail.com>
2020-11-01 16:02:44 +0530

    adding regex instead of if check

--- script.js
+++ script.js
@@ -5,23 +5,27 @@ let p = document.querySelector("p");
 
 let rand = Math.floor(Math.random() * 101); //generating number between 0 - 101
 
+let regex = /^[0-9]/gm;
+
 function check() {
   let inputVal = input.value; //getting value as a number
-  if (inputVal === "") {
-    //checking if the input field is not filled
-    p.innerHTML = "Please input a number";
+
+  if (regex.test(inputVal)) {
+    inputVal = +inputVal;
+  } else {
+    p.innerHTML = "Please input a valid number!";
+    p.style.color = "red";
     return;
   }
-  if (+inputVal === rand) {
+
+  if (inputVal === rand) {
     p.innerHTML = "Hurray! You guessed it right";
   } else if (rand - inputVal < 10 && rand - inputVal > -10) {
     p.innerHTML = "You are too close";
-  } else if (+inputVal < rand) {
+  } else if (inputVal < rand) {
     p.innerHTML = "Your guess is lower than the number";
-  } else if (+inputVal > rand) {
+  } else if (inputVal > rand) {
     p.innerHTML = "Your guess is higher than the number";
-  } else {
-    p.innerHTML = "Please input a valid number";
   }
 }
 
